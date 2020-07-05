76dd8a33ddb7544125e9629cda2e8900bf5210ea
Collin <pentigrampeace@gmail.com>
2020-07-05 04:36:52 -0700

    Added functions to calculate mean and median to DataAnalyzer.java. And removed unnecessary continue statement.

--- src/application/DataAnalyzer.java
+++ src/application/DataAnalyzer.java
@@ -17,7 +17,7 @@ public class DataAnalyzer {
                     float newFloat = Float.parseFloat(st);
                     dataArray.add(newFloat);
                 } catch (NumberFormatException e) {
-                    continue;
+                    // If value is not a float it is ignored.
                 }
             }
         } catch (FileNotFoundException e) {
@@ -35,6 +35,10 @@ public class DataAnalyzer {
         sortDataArray();
     }
 
+    public static int getDataArraySize(){
+        return dataArray.size();
+    }
+
     private static void sortDataArray(){
         Collections.sort(dataArray);
 
@@ -43,5 +47,28 @@ public class DataAnalyzer {
         }
         System.out.println("\n Total Count: " + dataArray.size());
     }
+
+    private static float getMean() {
+        float sum = 0f, mean;
+        for (float i: dataArray) {
+            sum += i;
+        }
+        mean = sum / dataArray.size();
+        return mean;
+    }
+
+    public static float getMedian(){
+        float median;
+
+        // If the size of the array is even, median is calculated by
+        // taking the sum of the middle two indices and dividing by 2
+        if(dataArray.size()%2 == 0) {
+             median = (dataArray.get(dataArray.size()/2) + dataArray.get(dataArray.size()/2 + 1))/2;
+        } else {
+            median = dataArray.get(dataArray.size()/2 + 1);
+        }
+        return median;
+    }
+
 
 }
