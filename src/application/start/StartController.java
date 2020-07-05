fa06d6dae08837c8cacc346d093c8dec121ed005
Collin <pentigrampeace@gmail.com>
2020-07-04 23:33:32 -0700

    Changed StartView.fxml so there is no longer a text field. Modified button so that it will now open a file system dialog box to prompt user to find the file they wish to upload.

--- src/application/start/StartController.java
+++ src/application/start/StartController.java
@@ -1,16 +1,30 @@
 package application.start;
 
+import java.io.File;
 import java.io.IOException;
 
 import application.App;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
 public class StartController {
 
-	private App app;
+
+	FileChooser fc = new FileChooser();
+
+	@FXML
+	private Button browseButton;
 
 	@FXML
 	private void upload() throws IOException {
-		app.goMain();
+		Window window = browseButton.getScene().getWindow();
+		File file = fc.showOpenDialog(window);
+		if(file != null) {
+			System.out.println("File Found");
+		}
+
+		App.loadMain();
 	}
 }
